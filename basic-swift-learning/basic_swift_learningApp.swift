789f92891ca5411d8b97fd9f0cabb03a6337c8ec
Cristobal Andres Contreras Orellana <chrisloarryn@gmail.com>
2023-08-11 01:08:20 -0400

    Initial Commit

---
+++ basic-swift-learning/basic_swift_learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  basic_swift_learningApp.swift
+//  basic-swift-learning
+//
+//  Created by Cristóbal Contreras on 11-08-23.
+//
+
+import SwiftUI
+
+@main
+struct basic_swift_learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ basic-swift-learningUITests/basic_swift_learningUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  basic_swift_learningUITestsLaunchTests.swift
+//  basic-swift-learningUITests
+//
+//  Created by Cristóbal Contreras on 11-08-23.
+//
+
+import XCTest
+
+final class basic_swift_learningUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
