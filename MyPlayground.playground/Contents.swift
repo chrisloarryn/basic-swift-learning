df5801fad7bceec543a17bb0d22ff833baa06c4c
Cristobal Andres Contreras Orellana <chrisloarryn@gmail.com>
2023-08-11 01:18:10 -0400

    chore: add really basic types (learning)

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,29 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+// ------------------------------------------------------------------------
+// STRINGS AND INT
+// ------------------------------------------------------------------------
+
+
+var age = 27
+var name = "Cristobal"
+
+// let equals a constant
+let nickName = "chrisloarryn"
+
+//// ok
+// age = 35
+//// nook
+// nickName = age
+
+//// cannot re-assign value because it is a constant (let)
+// nickName = "chrisloarryn2"
+
+var candy = "100 Grand"
+
+print(nickName)
+
+// ------------------------------------------------------------------------
+//
+// ------------------------------------------------------------------------
+
