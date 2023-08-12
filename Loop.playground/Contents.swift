67bde911109d90f28a09769fef1f006f6748be90
Cristobal Andres Contreras Orellana <chrisloarryn@gmail.com>
2023-08-12 12:36:07 -0400

    feat: loops

--- Loop.playground/Contents.swift
+++ Loop.playground/Contents.swift
@@ -16,3 +16,24 @@ for i in movies {
 for _ in 1...5 {
     print("Hello!")
 }
+
+
+// Make an array of your favorite numbers. Then print you favorite in order, including what number in the order it is.
+// Lucky Numbers: 5, 7, 8
+// 1. 5
+// 2. 7
+// 3. 8
+
+let luckyNumbers = [3, 7, 13, 23, 244]
+
+var rank = 1
+for number in luckyNumbers {
+    print("\(rank). \(number)")
+    
+    rank += 1
+}
+
+
+for x in 0..<luckyNumbers.count {
+    print("\(x+1). \(luckyNumbers[x])"
+}
