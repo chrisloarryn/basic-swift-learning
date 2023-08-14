63c5b5ceadd5b094c08c411d716bffd9f228e7d0
Cristobal Andres Contreras Orellana <chrisloarryn@gmail.com>
2023-08-13 21:35:51 -0400

    feat: dictionaries

--- Dictionaries.playground/Contents.swift
+++ Dictionaries.playground/Contents.swift
@@ -1,3 +1,26 @@
 import Foundation
 
 // Dictionaries
+
+var nickWords: [String:String] = [
+    "zabang": "when you finally get your code right",
+    "hooha": "When you've been coding for 3 hours and still can't find out what's wrong"
+]
+
+nickWords["zabang"]
+
+
+var dogsAge: [String:Int] = [
+    "Fido": 4,
+    "Jane": 4,
+    "Sean": 4
+]
+
+dogsAge["Joe"] = 7
+
+dogsAge
+
+dogsAge.removeValue(forKey: "Fido")
+dogsAge.removeValue(forKey: "Sean")
+
+dogsAge
