972507c3ead95b9bea129383db471df9c1bf413b
Cristobal Andres Contreras Orellana <chrisloarryn@gmail.com>
2023-08-13 21:23:51 -0400

    feat: add tuples and sets

--- TuplesAndSets.playground/Contents.swift
+++ TuplesAndSets.playground/Contents.swift
@@ -1,3 +1,30 @@
 import Foundation
 
 // Tuples and Sets.
+
+var dog: (String, Int) = ("Larry", 8)
+
+dog.0
+dog.1
+
+
+var luckyNumbers: [Int] = [432, 5, 324, 32, 5, 134, 21, 312]
+
+/// we cannot assign directly an array of numbers to a Set
+// var luckyNumber: Set<[Int]> = luckyNumbers
+
+var luckyNumbersSet: Set = [432, 5, 324, 32, 5, 134, 21, 312]
+
+luckyNumbersSet.insert(44)
+luckyNumbersSet.contains(44)
+
+
+// Create a set of Strings of food
+var food: Set = ["Oreos", "Pizza", "Gyudon"]
+food.insert("Sushi")
+
+food
+
+food.remove("Sushi")
+
+food
