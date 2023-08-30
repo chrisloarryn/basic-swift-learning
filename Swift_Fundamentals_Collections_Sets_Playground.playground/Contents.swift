0b676da2c3bd2a9209b21ab0e8361ad4587494ea
Cristobal Andres Contreras Orellana <chrisloarryn@gmail.com>
2023-08-30 00:13:05 -0400

    feat: update code sets functions

--- Swift_Fundamentals_Collections_Sets_Playground.playground/Contents.swift
+++ Swift_Fundamentals_Collections_Sets_Playground.playground/Contents.swift
@@ -1,3 +1,54 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+//--------------//
+// Collections //
+//------------//
+
+// Sets: unordered collection of unique values/objects.
+var sports = Set<String>()
+print("sports set = \(sports)")
+
+// adding
+sports.insert("Soccer")
+sports.insert("Basketball")
+sports.insert("Baseball")
+print("sports = \(sports)")
+
+if sports.contains("Soccer") {
+    print("Sports contains Soccer")
+}
+
+// removing
+sports.remove("Baseball")
+print("sports = \(sports.count)")
+
+sports = ["Football", "Hockey", "Lacrosse"]
+print("sports = \(sports.count)")
+
+// empty check
+if sports.isEmpty {
+    print("sports set is empty")
+}
+
+// iteration
+// sorted vs. unsorted
+print("unsorted set elements")
+for sport in sports {
+    print("sport = \(sport)")
+}
+
+print("sorted set elements")
+for sport in sports.sorted() {
+    print("sport = \(sport)")
+}
+
+// operations
+// - union
+// - intersection
+// - subtracting
+// - symmetricDifference
+let oddNumbers: Set<Int> = [1,3,5,7,9]
+let evenNumbers: Set<Int> = [2,4,6,8]
+
+let unionSet = oddNumbers.union(evenNumbers)
+
