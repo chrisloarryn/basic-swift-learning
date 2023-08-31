e488e6a16beff73556d3fd207f37700e7cd7870c
Cristobal Andres Contreras Orellana <chrisloarryn@gmail.com>
2023-08-31 17:32:18 -0400

    Update Contents.swift

--- Swift_Fundamentals_Collections_Dictionaries_Playground.playground/Contents.swift
+++ Swift_Fundamentals_Collections_Dictionaries_Playground.playground/Contents.swift
@@ -62,6 +62,40 @@ for values in studetIdName.values.sorted() {
 }
 
 
+/// Exercise
+
+class Exercise {
+    
+    // Implement a function that takes an array of string elements as a parameter.
+    // The array will contain city names. The array may contain duplicates.
+    // The function should return a dictionary.
+    // The dictionary key should be String, the value should be Int.
+    // The key of the dictionary will be a city name and the value will be the number of times the city name appeared in the array.
+    static func createDictionary(from array: [String]) -> [String: Int] {
+        var dictionary: [String: Int] = [:]
+
+        for city in array {
+            if let count = dictionary[city] {
+                dictionary[city] = count + 1
+            } else {
+                dictionary[city] = 1
+            }
+        }
+
+        return dictionary
+    }
+    
+    // Implement a function that takes a dictionary where a key is String and value is an integer.
+    // The function also takes another parameter which corresponds to the key of the dictionary
+    // that you will need to remove from the dictionary. The function should return the updated dictionary.
+    static func remove(key: String, from dictionary: [String: Int]) -> [String: Int] {
+        var dictionary = dictionary
+        dictionary[key] = nil
+        return dictionary
+    }
+}
 
 
 
+let arrayNamesRepeated = ["nomu","nomu","macaco","mono","macaco","mono"]
+let keyValuePair = Exercise.createDictionary(from: arrayNamesRepeated)
