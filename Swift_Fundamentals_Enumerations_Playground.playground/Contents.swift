2265b6b5019ea51b31c576872d722ff10571e55f
Cristobal Andres Contreras Orellana <chrisloarryn@gmail.com>
2023-09-01 15:14:00 -0400

    feat: enumerations starting

--- Swift_Fundamentals_Enumerations_Playground.playground/Contents.swift
+++ Swift_Fundamentals_Enumerations_Playground.playground/Contents.swift
@@ -1,3 +1,23 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//---------------//
+// Enumerations //
+//-------------//
+// Astructure that groups related values together
+// Type safe
+// case can have Int, String, Double, Float type
+
+enum Color: Double {
+    case red
+    case green
+    case blue
+}
+
+var color: Color = .red
+
+switch color {
+case .red:
+    break
+default:
+    break
+}
