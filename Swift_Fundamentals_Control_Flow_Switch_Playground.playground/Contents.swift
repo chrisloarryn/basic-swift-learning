769d7602e903741be65d77336c8799e3dae1cd99
Cristobal Andres Contreras Orellana <chrisloarryn@gmail.com>
2023-09-01 02:39:20 -0400

    Update Contents.swift

--- Swift_Fundamentals_Control_Flow_Switch_Playground.playground/Contents.swift
+++ Swift_Fundamentals_Control_Flow_Switch_Playground.playground/Contents.swift
@@ -7,14 +7,6 @@ import UIKit
 
 var temperatureInC = -10 // 15 // 20
 
-switch condition {
-case a:
-    // statement
-case b:
-    // statement
-default:
-}
-
 while (temperatureInC < 40) {
     if temperatureInC < 0 {
         print("It's really cold today! \(temperatureInC)")
@@ -25,3 +17,46 @@ while (temperatureInC < 40) {
     }
     temperatureInC += 3
 }
+
+while (temperatureInC < 40) {
+    switch temperatureInC {
+    case ..<0:
+        print("It's really cold today! \(temperatureInC) C")
+    case 0..<20:
+        print("It's little warm today! \(temperatureInC) C")
+    default:
+        print("It's very warm! \(temperatureInC) C")
+    }
+    temperatureInC += 3
+}
+
+let character: Character = "a"
+switch character {
+case "a":
+    print("character is latin alphabet character \(character)")
+case "B":
+    print("character is upper case latin letter \(character)")
+default:
+    print("any other character \(character)")
+}
+
+
+// switch statement tuples
+// tuple: (Int, Int)
+let coordinate = (0, 0)
+switch coordinate {
+case (0,0):
+    print("the coordinate origin point")
+case (_, 0):
+    print("the coordinate is on x axis")
+case (0, _):
+    print("the coordinate is on y axis")
+case (_, _):
+    print("the coordinate is no in x or y")
+}
+
+// value binding
+
+// where clause
+
+// compound cases
